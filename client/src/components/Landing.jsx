f407aae307134340d16ec88d8a2704bd6551b62c
Tim Z <tjkz87@gmail.com>
2016-09-22 15:09:46 -0700

    add Landing page component

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Landing = () => <p>Landing Page. Hello World!</p>;
+
+export default Landing;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Landing from './components/Landing.jsx';
 
-const App = () => <p> Hello React!</p>;
+//const App = () => <Landing />;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+  render() {
+    return (
+      <Landing loggedIn={this.state.loggedIn} />
+    );
+  }
+}
 
 render(<App />, document.getElementById('app'));
