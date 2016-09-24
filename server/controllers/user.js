5bbd8ad2e7ff19d2b280e9cbc9d0f2f4a14c99bf
turbio <masongclayton@gmail.com>
2016-09-24 12:06:36 -0700

    add user controller

---
+++ server/controllers/user.js
@@ -0,0 +1,9 @@
+const User = require('../models/user.js');
+
+module.exports.signin = (req, res) => {
+  res.send('not implemented');
+};
+
+module.exports.signup = (req, res) => {
+  res.send('not implemented');
+};
