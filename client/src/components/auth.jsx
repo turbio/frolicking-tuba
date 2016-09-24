a635bab340a4d63453cd4bb5c9424fee773dfc9b
Teague Ashburn <teague@ashburn.digital>
2016-09-24 16:58:20 -0700

    Added login and singup form components

---
+++ client/src/components/auth.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Login from './login.jsx';
+import Signup from './signup.jsx';
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { signup: false };
+  }
+
+  // onLoginHandler({ username, password }) {
+  //   console.log('username: ', username, 'password: ', password);
+  // }
+
+  // onSignupHandler({ username, password, companyName }) {
+  //   console.log('username: ', username,
+  //               'password: ', password,
+  //               'companyName: ', companyName);
+  // }
+
+  toggleAuthView() {
+    this.setState({ signup: !this.state.signup });
+  }
+
+  render() {
+    if (this.state.signup) {
+      return (<Signup
+        callback={({ username, password }) => {
+          console.log('username: ', username, 'password: ', password);
+        }}
+        toggleAuthView={() => this.toggleAuthView()}
+      />);
+    }
+
+    return (<Login
+      callback={({ username, password, companyName }) => {
+        console.log('username: ', username,
+                    'password: ', password,
+                    'companyName: ', companyName);
+      }}
+      toggleAuthView={() => this.toggleAuthView()}
+    />);
+  }
+}
+
+
+export default Auth;
