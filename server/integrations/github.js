d2d9b2375d685783225e67b224652b969c7b7d1a
turbio <masongclayton@gmail.com>
2016-09-28 10:45:56 -0700

    ...

--- server/integrations/github.js
+++ server/integrations/github.js
@@ -43,6 +43,11 @@ module.exports.register = (req, res) => {
     });
   });
 
+  console.log(
+    `SENDING: client_id=${config.github.client_id}\
+    &client_secret=${config.github.secret}\
+    &code=${req.query.code}`);
+
   githubReq.write(
     `client_id=${config.github.client_id}\
     &client_secret=${config.github.secret}\
