e46423f4a32a6a0b0ddb83c9d528ced7d38ec8cd
turbio <masongclayton@gmail.com>
2016-09-27 17:27:21 -0700

    restructure testing

--- server/models/integration.js
+++ server/models/integration.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 const User = require('./user');
+const config = require('../../env/config.json');
 
 const Integration = db.define('integration', {
-  type: { type: Sequelize.STRING },
-  meta: { type: Sequelize.STRING }
+  type: Reflect.apply(Sequelize.ENUM, null, config.integrations),
+  meta: Sequelize.STRING
 });
 
 Integration.belongsTo(User);
