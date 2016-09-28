989123ed472763d3bbe31c412b0d35b1b2eed2e8
Teague Ashburn <teague@ashburn.digital>
2016-09-28 11:57:22 -0700

    Updated format of github.createIssue

--- server/controllers/annotate.js
+++ server/controllers/annotate.js
@@ -4,8 +4,11 @@ module.exports.create = (req, res) => {
   console.log('got annotation', req.body);
 
   github.createIssue('frolicking-tuba/issue-tester', {
-    title: req.body.comment,
-    body: `>${req.body.selected}`
+    title: req.body.title,
+    body: `#to: ${req.body.to} \n
+      #from: ${req.body.from} \n
+      #selected text: ${req.body.selected} \n
+      #comment: ${req.body.comment}`
   });
 
   res.end();
