ef512f0c5e9a1a95bc7d50d10f04da00992c3018
xsvfat <xsvfat@gmail.com>
2016-09-28 15:07:18 -0700

    Add skeleton for dashbaord

--- client/src/components/Documentation.jsx
+++ client/src/components/Documentation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Swagger from 'swagger-client';
+// import Swagger from 'swagger-client';
 // import AutoAffix from 'react-overlays';
 // import Waypoint from 'react-waypoint';
 
@@ -15,16 +15,16 @@ const Documentation = () => (
 );
 
 
-const client = new Swagger({
-  url: 'http://localhost:3000/api-docs.json',
-  supportHeaderParams: false,
-  supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
-  success: () => {
-    client((test) => {
-      console.log('pet', test);
-    });
-  }
-});
+// const client = new Swagger({
+//   url: 'http://localhost:3000/api-docs.json',
+//   supportHeaderParams: false,
+//   supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
+//   success: () => {
+//     client((test) => {
+//       console.log('pet', test);
+//     });
+//   }
+// });
 
 export default Documentation;
 
