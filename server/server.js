6ff088e35bebe4ea55c311455f9456566765d28d
turbio <masongclayton@gmail.com>
2016-09-29 21:05:18 -0700

    server index.html on all non routed urls

--- server/server.js
+++ server/server.js
@@ -4,18 +4,11 @@ const session = require('express-session');
 
 const config = require('../env/config.json');
 const routes = require('./routes');
-//const swagger = require('swagger-express');
-
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// app.all('/*', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
 
 app.set('trust proxy', 1);
 app.use(session({
@@ -29,22 +22,7 @@ app.use(session({
   }
 }));
 
-
-// app.use(swagger.init(app, {
-//   apiVersion: '1.0',
-//   swaggerVersion: '1.2',
-//   swaggerURL: '/swagger',
-//   swaggerJSON: '/api-docs.json',
-//   swaggerUI: './public/swagger/',
-//   basePath: 'http://localhost:3000',
-//   apis: ['./server/api/github.js'],
-//   middleware: () => {}
-// }));
-
-
 app.use(routes);
-app.use(express.static('./client/public'));
-
 
 app.listen(config.server.port, () => {
   console.log(`started on port ${config.server.port}`);
--- test/server.js
+++ test/server.js
@@ -3,6 +3,41 @@ const request = require('supertest');
 const Integration = require('../server/models/integration');
 
 describe('server', () => {
+  describe('static files', () => {
+    it('should serve index.html at all urls', (done) => {
+      request(server)
+        .get('/fdajsklfdas')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .then(() => {
+          request(server)
+            .get('/aaaaaaaaaa/a/a/a/aaaatest')
+            .expect('Content-Type', /html/)
+            .expect(200);
+        }).then(() => {
+          request(server)
+            .get('/not_a_real_url')
+            .expect('Content-Type', /html/)
+            .expect(200)
+            .end(done);
+        });
+    });
+    it('should serve /index.html', (done) => {
+      request(server)
+        .get('/')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .end(done);
+    });
+    it('should serve /bundle.js', (done) => {
+      request(server)
+        .get('/bundle.js')
+        .expect('Content-Type', /javascript/)
+        .expect(200)
+        .end(done);
+    });
+  });
+
   describe('/script.js', () => {
     it('should GET /script.js', (done) => {
       request(server)
