ac2cf30d5ef384446619c71d322d4b8a62062be5
turbio <masongclayton@gmail.com>
2016-10-01 14:00:56 -0700

    load apiscript on server start

--- server/controllers/script.js
+++ server/controllers/script.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 
 let script = null;
 
+fs.readFile('./apiscript/markup.min.js', 'utf8', (err, src) => {
+  if (err) {
+    console.log(err);
+  }
+
+  script = src;
+});
+
 module.exports.get = (req, res) => {
   if (!req.query.key) {
     res.status(400).end('you must include a key');
@@ -10,16 +18,5 @@ module.exports.get = (req, res) => {
   }
 
   res.contentType('application/javascript');
-
-  if (script) {
-    res.send(script.replace('%KEY%', req.query.key));
-  } else {
-    fs.readFile('./apiscript/markup.min.js', 'utf8', (err, src) => {
-      if (err) {
-        console.log(err);
-      }
-      script = src;
-      res.send(script);
-    });
-  }
+  res.send(script.replace('%KEY%', req.query.key));
 };
