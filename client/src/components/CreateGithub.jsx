9c2b90141e94f7e852e2b582b60c9a89603ce551
Tim Z <tjkz87@gmail.com>
2016-09-30 17:32:28 -0700

    add repo select and redirect to dashboard

--- client/src/components/CreateGithub.jsx
+++ client/src/components/CreateGithub.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { FormGroup, FormControl, Button, Row, Col } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
+
 
 class CreateGithub extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       githubRepos: ['repo1', 'repo2'],
-      githubAuth: false
+      githubAuth: false,
+      sel_repo: ''
     };
+    this.onClickHandler = this.onClickHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
   }
 
   componentWillMount() {
     this.getGithubAuth();
+  }
+
+  onClickHandler(event) {
+    console.log('onClickHandler called!');
+    event.preventDefault();
+    this.setGithubRepo();
+  }
+
+  onChangeHandler(event) {
+    this.setState({ sel_repo: event.target.value });
   }
 
   getGithubAuth() {
@@ -35,8 +50,37 @@ class CreateGithub extends React.Component {
     .then((json) => {
       console.log('json:', json);
       this.setState({ githubRepos: json });
+      this.setState({ sel_repo: json[0] });
     })
     .catch((error) => console.log('fetch repos', error));
+  }
+
+  setGithubRepo() {
+    // ajax call to /api/integrations/github/repos POST
+    // javascript { "name": String }
+    console.log('setGithubRepo called!');
+
+    const data = { name: this.state.sel_repo };
+
+    const headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+
+    fetch('/api/integrations/github/repos', {
+      method: 'POST',
+      headers,
+      credentials: 'same-origin',
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.error) {
+        console.log('json:', json);
+      } else {
+        browserHistory.push('/dashboard');
+      }
+    })
+    .catch((error) => console.log('set repo error', error));
   }
 
   render() {
@@ -44,7 +88,11 @@ class CreateGithub extends React.Component {
     const dropdownSelect = (
       <form>
         <FormGroup>
-          <FormControl componentClass="select" placeholder="select">
+          <FormControl
+            componentClass="select"
+            placeholder="select"
+            onChange={this.onChangeHandler}
+          >
             {
               this.state.githubRepos.map((repo) => <option value={repo}>
                 {repo}
@@ -52,7 +100,7 @@ class CreateGithub extends React.Component {
             }
           </FormControl>
         </FormGroup>
-        <Button type="submit">
+        <Button type="submit" onClick={this.onClickHandler} bsStyle="primary">
           Submit
         </Button>
       </form>
