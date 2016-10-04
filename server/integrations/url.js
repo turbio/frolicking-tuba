29c3498f13c7a48f7a0082e3a87817794f63d0c2
Tim Z <tjkz87@gmail.com>
2016-10-04 10:58:54 -0700

    complete url integrations controller

--- server/integrations/url.js
+++ server/integrations/url.js
@@ -1,4 +1,7 @@
 const config = require('../../env/config.json');
+const Integration = require('../models/integration');
+const Output = require('../models/output');
+const Key = require('../models/key');
 
 module.exports.urlList = (req, res) => {
   if (!req.session.user) {
@@ -6,6 +9,26 @@ module.exports.urlList = (req, res) => {
 
     return;
   }
+
+  Integration.findOne({
+    where: {
+      userId: req.session.user.id,
+      type: 'url'
+    }
+  })
+  .then((integration) => {
+    if (!integration) {
+      res.status(400).json([]);
+
+      return;
+    }
+
+    Output.findAll({ where: { integrationId: integration.id } })
+    .then((urls) => {
+      res.status(200).json(urls.map((url) => url.meta));
+    });
+
+  });
 };
 
 module.exports.urlSelect = (req, res) => {
@@ -14,4 +37,39 @@ module.exports.urlSelect = (req, res) => {
 
     return;
   }
+
+  let newIntegrationId = false;
+
+  Integration.findOne({
+    where: {
+      userId: req.session.user.id,
+      type: 'url'
+    }
+  })
+  .then((integration) => {
+    if (!integration) {
+      return Integration.create({
+        userId: req.session.user.id,
+        type: 'url'
+      });
+    }
+
+    return integration;
+  })
+  .then((integration) => {
+    newIntegrationId = integration.id;
+
+    return Key.create({ userId: req.session.user.id });
+  })
+  .then((key) => Output.create({
+    meta: req.body.name,
+    integrationId: newIntegrationId,
+    keyId: key.id
+  }))
+  .then((output) => {
+    res.send(output);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
 };
