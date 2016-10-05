e7402b45cdcf9b184d62d6c0c974572b501a665d
Tim Z <tjkz87@gmail.com>
2016-10-04 18:58:06 -0700

    refactor annotate to handle both github and post to URL

--- server/controllers/annotate.js
+++ server/controllers/annotate.js
@@ -1,6 +1,10 @@
 const github = require('../integrations/github');
 const config = require('../../env/config.json');
 
+const Integration = require('../models/integration');
+const Output = require('../models/output');
+const Key = require('../models/key');
+
 const accessHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
@@ -20,17 +24,33 @@ module.exports.create = (req, res) => {
     return;
   }
 
-  github.createIssue(
-    req.body.key,
-    {
-      title: req.body.title,
-      body:
-        `#to: ${req.body.to}\n`
-        + `#from: ${req.body.from}\n`
-        + `#selected text: ${req.body.selected}\n`
-        + `#comment: ${req.body.comment}`
-    });
+  const params = {
+    type: '',
+    integration_meta: '',
+    output_meta: ''
+  };
 
-  res.set(accessHeaders);
-  res.end();
+  Key.findOne({
+    where: { key: req.body.key },
+    include: [Output]
+  })
+  .then((key) => {
+    params.output_meta = key.output.meta;
+
+    return Integration.findOne({ where: { id: key.output.integrationId } });
+  })
+  .then((integration) => {
+    params.type = integration.type;
+    params.integration_meta = integration.meta;
+
+    if (integration.type === 'github') {
+      github.createIssue(params, req.body);
+    }
+    // if (integration.type === 'url') {
+    //   url.postToUrl(params, req.body);
+    // }
+
+    res.set(accessHeaders);
+    res.end();
+  });
 };
