d592e278d5df1dda70822785ae7cabb9ee9b7a97
turbio <masongclayton@gmail.com>
2016-10-06 12:01:01 -0700

    dashboard should be stateless

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,63 +6,18 @@ import { connect } from 'react-redux';
 import Key from './Key.jsx';
 import * as Actions from '../actions/AppActions';
 
-class Dashboard extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     keys: [{
-  //       name: 'MyRow',
-  //       api_key: 'somestring',
-  //       endpoint: 'somenedpoint'
-  //     },
-  //     {
-  //       name: 'MyRow2',
-  //       api_key: 'somestring2',
-  //       endpoint: 'somenedpoint2'
-  //     }]
-  //   };
-  // }
+const Dashboard = () => (
+  <Grid>
+    <Row>
+      <p><Link to="/create">Create Key</Link></p>
+    </Row>
+    <Row>
+      <Key title="key name" endpoint="key endpoint" keyString="api key" />
+    </Row>
+  </Grid>
+);
 
-  componentDidMount() {
-    this.props.getApiKeys();
-  }
-
-  // getApiKeys() {
-  //   fetch('/api/keys', { credentials: 'same-origin' })
-  //   .then((response) => response.json())
-  //   .then((json) => {
-  //     const keys = json.map((key) => {
-  //       const newKey = key;
-
-  //       newKey.api_key = `<script src="http://getmarkup.com/script.js?key=\
-  //                         ${key.api_key}"></script>`;
-
-  //       return newKey;
-  //     });
-
-  //     this.setState({ keys });
-  //   })
-  //   .catch((error) => console.log('fetch /api/keys error:', error));
-  // }
-
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <p><Link to="/create">Create Key</Link></p>
-        </Row>
-        <Row>
-          <Key title="key name" endpoint="key endpoint" keyString="api key" />
-        </Row>
-      </Grid>
-    );
-  }
-}
-
-Dashboard.propTypes = {
-  getApiKeys: PropTypes.func,
-  keys: PropTypes.arrayOf(PropTypes.object)
-};
+Dashboard.propTypes = { keys: PropTypes.arrayOf(PropTypes.object) };
 
 
 const mapStateToProps = (state) => ({ keys: state.apiKeys.keys });
