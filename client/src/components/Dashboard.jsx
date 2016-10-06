addf01f09933c9ceeb05c4a37832821f5aa59761
turbio <masongclayton@gmail.com>
2016-10-06 11:46:43 -0700

    small component changes

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Grid, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
-import DashboardTable from './DashboardTable.jsx';
+import Key from './Key.jsx';
 import * as Actions from '../actions/AppActions';
 
 class Dashboard extends Component {
@@ -48,15 +48,12 @@ class Dashboard extends Component {
   render() {
     return (
       <Grid>
-        <Row className="mainLanding">
-          <Col xs={12} md={12}>
-            <h3>YOUR SCRIPT TAG</h3>
-            <p>Copy and paste the script tag below into your html body.</p>
-            <p><Button bsStyle="primary">Learn more</Button></p>
-            <p><Link to="/create">Create Key</Link></p>
-          </Col>
+        <Row>
+          <p><Link to="/create">Create Key</Link></p>
         </Row>
-        <DashboardTable keys={this.props.keys} />
+        <Row>
+          <Key title="key name" endpoint="key endpoint" keyString="api key" />
+        </Row>
       </Grid>
     );
   }
@@ -72,6 +69,3 @@ const mapStateToProps = (state) => ({ keys: state.apiKeys.keys });
 
 //export default Dashboard;
 export default connect(mapStateToProps, Actions)(Dashboard);
-
-//convert html code to text.html
-//http://www.freebits.co.uk/convert-html-code-to-text.html
