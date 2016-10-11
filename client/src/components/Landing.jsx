d333ca353d24e4061ca1b1451257d60eb83c3a86
Teague Ashburn <teague@ashburn.digital>
2016-10-11 16:05:55 -0700

    Landing page styling in-progress

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Row, Col, Grid, Button } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Grid,
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  Image
+} from 'react-bootstrap';
 
 const Landing = () => (
   <div>
@@ -24,31 +32,44 @@ const Landing = () => (
         </linearGradient>
       </defs>
       <path
-        d="M0,0 L1024,0 C1024,0 1026.6096,384 1024,384 C950,384 647.47765,432.199 407,499 C165.899454,565.974031 0,650 0,650 L0,0 Z"  // eslint-disable-line
+        d="M0,0 L1024,0 L1024,376 L1024,382 L1024,384 C950,384 647.47765,432.199 407,499 C165.899454,565.974031 0,650 0,650 L0,0 Z"  // eslint-disable-line
         id="Rectangle-2"
         stroke="none"
         fill="url(#linearGradient-1)"
         fillRule="evenodd"
       />
     </svg>
-    <Grid>
+    <Grid className="landingPageGrid">
       <Row className="mainLanding">
-        <Col xs={12} md={12}>
+        <Col xs={1} md={1} />
+        <Col xs={11} md={6}>
           <div className="landingBackground">
             <div className="main-area">
-              <h3>Landing Page. Hello World!</h3>
-              <p>This is the Content.</p>
-              <p><Button bsStyle="primary">Learn more</Button></p>
+              <h3 className="homeHeader">Markup for a seamless feedback workflow</h3>
+              <p className="homePara">GetMarkup is an efficient way to collect feedback from your website, and have it automatically routed to GitHub issues or your own custom endpoint.</p>
+              <p>
+                <ButtonToolbar>
+                  <ButtonGroup>
+                    <Button bsStyle="primary" className="button1">Sign Up</Button>
+                  </ButtonGroup>
+                  <ButtonGroup>
+                    <Button bsStyle="primary" className="button2">Read Docs</Button>
+                  </ButtonGroup>
+                </ButtonToolbar>
+              </p>
             </div>
           </div>
+        </Col>
+        <Col xs={0} md={5}>
+          <Image src="Markup_Landingpage_Image.png" thumbnail className="mainLandingImg"/>
         </Col>
       </Row>
 
       <Row className="section1">
         <Col xs={12} md={4}>
           <div className="section1Container1">
-            <h3>section1Container1</h3>
-            <p>This is the Content.</p>
+            <h3 className="homeHeader">Easy Setup</h3>
+            <p className="homePara">This is the Content.</p>
           </div>
         </Col>
         <Col xs={12} md={4}>
@@ -68,7 +89,7 @@ const Landing = () => (
       <Row className="section2">
         <Col xs={12} md={8}>
           <div className="section2Container1">
-            <h3>section2Container1</h3>
+            <h3>Capture Feedback Quickly</h3>
             <p>This is the Content.</p>
           </div>
         </Col>
@@ -83,7 +104,7 @@ const Landing = () => (
       <Row className="section3">
         <Col xs={12} md={4}>
           <div className="section3Container1">
-            <h3>section3Container1</h3>
+            <h3>Workflow Options</h3>
             <p>This is the Content.</p>
           </div>
         </Col>
@@ -99,7 +120,7 @@ const Landing = () => (
         <Col xs={12} md={12}>
           <div className="finalCTA">
             <div className="main-area">
-              <h3>Final CTA AREA</h3>
+              <h3>Try Markup Today</h3>
               <p>This is the Content.</p>
               <p><Button bsStyle="primary">Learn more</Button></p>
             </div>
