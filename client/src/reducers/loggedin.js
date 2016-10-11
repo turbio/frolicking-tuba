2086ae6bb5538860d7402bca06484588bab0ba5e
Tim Z <tjkz87@gmail.com>
2016-10-10 19:46:32 -0700

    add email to auth state

--- client/src/reducers/loggedin.js
+++ client/src/reducers/loggedin.js
@@ -3,6 +3,7 @@ import { AUTH_USER, SIGN_OUT_USER, AUTH_ERROR } from '../utils/AppConstants';
 // The initial application state
 const initialState = {
   authenticated: false,
+  email: '',
   error: null
 };
 const assign = Object.assign;
@@ -11,7 +12,11 @@ const assign = Object.assign;
 export default function authReducer(state = initialState, action) {
   switch (action.type) {
   case AUTH_USER:
-    return assign({}, state, { authenticated: true, error: null });
+    return assign({}, state, {
+      authenticated: true,
+      email: action.email,
+      error: null
+    });
   case SIGN_OUT_USER:
     return assign({}, state, initialState);
   case AUTH_ERROR:
