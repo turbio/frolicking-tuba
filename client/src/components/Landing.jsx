2ac8a3cfef6e102915aa3a0b9803b32de9db553d
Teague Ashburn <teague@ashburn.digital>
2016-10-12 10:20:45 -0700

    Homepage and navbar updates

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -11,34 +11,7 @@ import {
 
 const Landing = () => (
   <div>
-    <svg
-      className="swoosh"
-      viewBox="0 0 1031 650"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-    >
-      <defs>
-        <linearGradient
-          x1="100%"
-          y1="50%"
-          x2="0%"
-          y2="50%"
-          id="linearGradient-1"
-        >
-          <stop stopColor="#EE433D" offset="0%" />
-          <stop stopColor="#EC3846" offset="49.6568765%" />
-          <stop stopColor="#EC2C51" offset="100%" />
-        </linearGradient>
-      </defs>
-      <path
-        d="M0,0 L1024,0 L1024,376 L1024,382 L1024,384 C950,384 647.47765,432.199 407,499 C165.899454,565.974031 0,650 0,650 L0,0 Z"  // eslint-disable-line
-        id="Rectangle-2"
-        stroke="none"
-        fill="url(#linearGradient-1)"
-        fillRule="evenodd"
-      />
-    </svg>
+    <Image className="swoosh" src="swoosh.svg" />
     <Grid className="landingPageGrid">
       <Row className="mainLandingSpacer" />
       <Row className="mainLanding">
@@ -66,20 +39,26 @@ const Landing = () => (
       </Row>
 
       <Row className="section1">
+        <Col xs={12} md={12}>
+          <h3 className="sectionHeader">Collaborate Better Across Teams</h3>
+        </Col>
         <Col xs={12} md={4}>
           <div className="section1Container1">
-            <h3 className="homeHeader">Easy Setup</h3>
-            <p className="homePara">This is the Content.</p>
+            <div className="rectanglePlaceholder" />
+            <h3>Easily Set Up New Projects</h3>
+            <p>section1Container1</p>
           </div>
         </Col>
         <Col xs={12} md={4}>
           <div className="section1Container2">
+            <div className="rectanglePlaceholder" />
             <h3>section1Container2</h3>
             <p>This is the Content.</p>
           </div>
         </Col>
         <Col xs={12} md={4}>
           <div className="section1Container3">
+            <div className="rectanglePlaceholder" />
             <h3>section1Container3</h3>
             <p>This is the Content.</p>
           </div>
