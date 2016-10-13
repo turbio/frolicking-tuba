2b8db2db9c291e70b59d9d1c4c52c96f98b95ccb
Tim Z <tjkz87@gmail.com>
2016-10-12 19:02:53 -0700

    add EditKeyModal component

---
+++ client/src/components/EditKeyModal.jsx
@@ -0,0 +1,192 @@
+import React, { PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
+import { Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import * as Actions from '../actions/AppActions';
+
+class EditKeyModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onDropdownChange = this.onDropdownChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    const keyName = findDOMNode(this.keyName).value;
+
+    if (this.urlText) {
+      this.props.createNewKey({
+        key: this.props.editKey,
+        name: keyName,
+        endpoint: findDOMNode(this.urlText).value,
+        type: 'url'
+      });
+    } else {
+      const requestBody = JSON.parse(findDOMNode(this.endpointSelect).value);
+
+      requestBody.name = keyName;
+      requestBody.key = this.props.editKey;
+      this.props.createNewKey(requestBody);
+    }
+  }
+
+  onDropdownChange() {
+    const selectValue = findDOMNode(this.endpointSelect).value;
+
+    if (selectValue === 'addnewurl') {
+      this.props.setEditModalNewUrl(true);
+    } else {
+      this.props.setEditModalNewUrl(false);
+    }
+  }
+
+  render() {
+    let showTextInput = !(this.props.github || (this.props.urls.length > 0));
+
+    if (!showTextInput) {
+      showTextInput = this.props.modalModeAddUrl;
+    }
+
+    let defaultValues = this.props.keys.reduce((prev, key) => {
+      let values = prev;
+
+      if (values === null && key.key === this.props.editKey) {
+        values = {
+          name: key.name,
+          endpoint: `{"type":"${key.type}","endpoint":"${key.endpoint}"}`
+        };
+      }
+
+      return values;
+    }, null);
+
+    defaultValues = defaultValues || {};
+
+    const urlTextInput = (
+      <FormGroup controlId="formBasicText">
+        <ControlLabel>URL</ControlLabel>
+        <FormControl
+          type="text"
+          placeholder="Enter URL"
+          ref={(ref) => { this.urlText = ref; }}
+        />
+      </FormGroup>
+    );
+
+    const dropdown = (
+      <FormGroup controlId="formControlsSelect">
+        <ControlLabel>Select Endpoint</ControlLabel>
+        <FormControl
+          componentClass="select"
+          defaultValue={defaultValues.endpoint}
+          ref={(ref) => { this.endpointSelect = ref; }}
+          onChange={this.onDropdownChange}
+        >
+          {
+            this.props.urls.map((url) => (
+              <option
+                value={`{"type":"url","endpoint":"${url.url}"}`}
+                key={url.url}
+              >
+                {url.url}
+              </option>)
+            )
+          }
+          {
+            this.props.repos.map((repo) => (
+              <option
+                value={`{"type":"github","endpoint":"${repo.full_name}"}`}
+                key={repo.full_name}
+              >
+                {repo.full_name}
+              </option>
+            ))
+          }
+          <option value="addnewurl">Add a new URL</option>
+        </FormControl>
+      </FormGroup>
+    );
+
+    const authGithubButton = (
+      <div>
+        <div>
+          Or
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={(event) => {
+            event.preventDefault();
+            document.location = '/api/integrations/github';
+          }}
+        >
+          Link Github Account
+        </button>
+        <br />
+        <br />
+      </div>
+    );
+
+
+    return (
+      <Modal
+        show={this.props.show}
+        onHide={() => {
+          this.props.setEditModalNewUrl(false);
+          this.props.hideEditModal();
+        }}
+      >
+        <Modal.Header closeButton />
+        <Modal.Body>
+          <form>
+            <FormGroup controlId="keyn">
+              <ControlLabel>Name</ControlLabel>
+              <FormControl
+                type="text"
+                defaultValue={defaultValues.name}
+                ref={(ref) => { this.keyName = ref; }}
+              />
+            </FormGroup>
+            { !this.props.github
+              && this.props.urls.length === 0 ? '' : dropdown }
+            { showTextInput ? urlTextInput : '' }
+            { this.props.github ? '' : authGithubButton }
+            <button
+              action="submit"
+              className="btn btn-primary"
+              onClick={this.onFormSubmit}
+            >
+              Update Key
+            </button>
+          </form>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
+
+EditKeyModal.propTypes = {
+  show: PropTypes.bool,
+  editKey: PropTypes.string,
+  modalModeAddUrl: PropTypes.bool,
+  keys: PropTypes.arrayOf(PropTypes.any),
+  github: PropTypes.bool,
+  urls: PropTypes.arrayOf(PropTypes.any),
+  repos: PropTypes.arrayOf(PropTypes.any),
+  hideEditModal: PropTypes.func,
+  setEditModalNewUrl: PropTypes.func,
+  createNewKey: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  show: state.editmodal.show,
+  editKey: state.editmodal.key,
+  modalModeAddUrl: state.editmodal.modalModeAddUrl,
+  keys: state.keys.data,
+  github: state.auth.github,
+  urls: state.urls.urls,
+  repos: state.repos.repos
+});
+
+export default connect(mapStateToProps, Actions)(EditKeyModal);
